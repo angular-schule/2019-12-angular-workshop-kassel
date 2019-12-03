cc150780b5ecb214b93f3a82aaedab6bb90f21d7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-12-03 16:49:14 +0100

    shallow unit test

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,8 +10,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      schemas: [NO_ERRORS_SCHEMA] // shallow unit test
     }).compileComponents();
   }));
 
@@ -23,13 +25,13 @@ describe('AppComponent', () => {
   it(`should have as title 'book-rating'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title).toEqual('Book Rating');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('book-rating app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Book Rating');
   });
 });
