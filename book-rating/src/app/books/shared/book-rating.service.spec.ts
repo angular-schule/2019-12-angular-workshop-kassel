236063b1482fbb0f31da377bd6c3681c4e29276f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-12-03 14:20:49 +0100

    tdd example, finished

--- book-rating/src/app/books/shared/book-rating.service.spec.ts
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -1,12 +1,45 @@
-import { TestBed } from '@angular/core/testing';
-
 import { BookRatingService } from './book-rating.service';
+import { Book } from './book';
 
 fdescribe('BookRatingService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: BookRatingService = TestBed.get(BookRatingService);
-    expect(service).toBeTruthy();
+  let service: BookRatingService;
+  let book: Book;
+
+  beforeEach(() => {
+    service = new BookRatingService();
+    book = {
+      isbn: '000',
+      title: 'Test',
+      description: '',
+      rating: 3
+    };
+  });
+
+  it('[JIRA-1213] should rate up a book by one', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
+  });
+
+  it('should rate down a book by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('should always return a new book instance (assuming immutability', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook).not.toBe(book);
   });
 });
--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Book } from './book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookRatingService {
 
-  constructor() { }
+  readonly minRating = 1;
+  readonly maxRating = 5;
+
+  rateUp(book: Book): Book {
+    return {
+      ...book,
+      rating: book.rating < this.maxRating ? book.rating + 1 : this.maxRating
+    };
+  }
+
+  rateDown(book: Book): Book {
+    const rating = Math.max(book.rating - 1, this.minRating);
+    return {
+      ...book, // spread operator
+      rating
+    };
+  }
+
+  // destructuring
+  rateMaxUp = ({ isbn, title, description }: Book) => ({
+    isbn,
+    title,
+    description,
+    rating: 5,
+    ...(true ? { xxx: 1 } : {}) // optional property
+  })
 }
