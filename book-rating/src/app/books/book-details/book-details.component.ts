9ef4b3487b4fba43a4baf1052f568f9e2721026e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-12-04 17:00:51 +0100

    introducing async pipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  isbn: string;
+  isbn$ = this.route.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn'))
+  );
 
   constructor(private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.isbn = this.route.snapshot.paramMap.get('isbn');
-  }
 }
