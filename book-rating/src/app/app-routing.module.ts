860be501043e2948de6e7df7e8ca6b9bcfa617dc
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-12-04 16:38:07 +0100

    introducing  routerLink

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // preloadingStrategy: PreloadAllModules, // with lazy loading
+    scrollPositionRestoration: 'enabled',
+    // enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
