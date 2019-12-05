3a8e10213ecf2168132c15c53a6b3f2cb1ef24af
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-12-05 09:31:59 +0100

    rxjs: subscriptions

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, interval, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 
@@ -9,11 +9,13 @@ import { map } from 'rxjs/operators';
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements OnInit, OnDestroy {
 
   isbn$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn'))
   );
+
+  sub2: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -26,6 +28,15 @@ export class BookDetailsComponent implements OnInit {
       complete: () => console.log('COMPLETE!')
     };
 
-    of('😎', '🧐', '🤠', '🤮').subscribe(observer);
+    const subscription = of('😎', '🧐', '🤠', '🤮').subscribe(observer);
+    subscription.unsubscribe();
+
+    // warnung! leak
+    this.sub2 = interval(1000).subscribe(e => console.log(e));
+
+  }
+
+  ngOnDestroy() {
+    this.sub2.unsubscribe();
   }
 }
